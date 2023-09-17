6d3fe5a11277e31a5b3574a214e6aafda837dfd3
Ben Shabowski <benshabowski@gmail.com>
2023-09-17 09:54:14 -0500

    Removing unused imports

--- src/main/java/bot/listener/DevopsBot.java
+++ src/main/java/bot/listener/DevopsBot.java
@@ -1,18 +1,12 @@
 package bot.listener;
 
-import application.App;
 import com.zgamelogic.jda.AdvancedListenerAdapter;
 import lombok.extern.slf4j.Slf4j;
-import net.dv8tion.jda.api.interactions.commands.build.CommandData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.LinkedList;
-
-import static com.zgamelogic.jda.Annotations.*;
 
 @Slf4j
 @RestController
@@ -20,7 +14,7 @@ public class DevopsBot extends AdvancedListenerAdapter {
 
     @Autowired
     public DevopsBot(){
-//        getCommands().add();
+
     }
 
     @GetMapping("health")
