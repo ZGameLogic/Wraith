8ede349af4e17ddd381aee6cc7ede72029d39e0c
Ben Shabowski <benshabowski@gmail.com>
2023-10-05 13:17:10 -0500

    Saving jsons betterly

--- src/main/java/bot/listener/DevopsBot.java
+++ src/main/java/bot/listener/DevopsBot.java
@@ -10,15 +10,23 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
 @Slf4j
 @RestController
 public class DevopsBot extends AdvancedListenerAdapter {
 
     private final GithubRepository githubRepository;
+    private final static File DATA_DIR = new File("jsons");
 
     @Autowired
     public DevopsBot(GithubRepository githubRepository){
         this.githubRepository = githubRepository;
+        if(!DATA_DIR.exists()) {
+            DATA_DIR.mkdirs();
+        }
     }
 
     @GetMapping("health")
@@ -27,13 +35,16 @@ public class DevopsBot extends AdvancedListenerAdapter {
     }
 
     @PostMapping("github")
-    private void github(@RequestBody String body){
+    private void github(@RequestBody String body) throws FileNotFoundException {
         JSONObject jsonBody = new JSONObject(body);
-        switch(jsonBody.getString("action")){
-            case "created":
-                break;
-            case "deleted":
-                break;
-        }
+        String action = jsonBody.getString("action");
+        File actionDir = new File(DATA_DIR.getPath() + "/" + action);
+        int suffix = actionDir.listFiles() == null ? 0 : actionDir.listFiles().length;
+        File json = new File(actionDir.getPath() + "/" + action + "-" + suffix + ".json");
+        if(!actionDir.exists()) actionDir.mkdirs();
+        PrintWriter out = new PrintWriter(json);
+        out.println(jsonBody.toString(4));
+        out.flush();
+        out.close();
     }
 }
