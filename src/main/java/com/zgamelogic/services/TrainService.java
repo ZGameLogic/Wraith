a8fb6360169ac1fa02ff9adc070cfbc136098c4d
Ben Shabowski <benshabowski@gmail.com>
2025-02-16 10:50:50 -0600

    start

--- src/main/java/com/zgamelogic/services/TrainService.java
+++ src/main/java/com/zgamelogic/services/TrainService.java
@@ -1,8 +1,14 @@
 package com.zgamelogic.services;
 
 import com.zgamelogic.data.database.trains.*;
+import jakarta.annotation.PostConstruct;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalTime;
+import java.util.List;
+
+import static com.zgamelogic.data.database.trains.Train.Bound.*;
 
 @Service
 @AllArgsConstructor
@@ -10,4 +16,13 @@ public class TrainService {
     private final TrainRepository trainRepository;
     private final TrainLineRepository trainLineRepository;
     private final TrainStopRepository trainStopRepository;
+
+    @PostConstruct
+    public void init(){
+        getTrains(LocalTime.of(8, 0), "UPW").forEach(train -> System.out.println(train.getId().getNumber()));
+    }
+
+    public List<Train> getTrains(LocalTime time, String line) {
+        return trainRepository.findByBoundAndLineAndStopAfterTime(time, line, OUTBOUND);
+    }
 }
