82951c5806877b98b3d252f229c27fa82d91a45c
Ben Shabowski <benshabowski@gmail.com>
2025-08-21 12:08:22 -0500

    more modrinth stuff

--- src/main/java/com/zgamelogic/services/ModrinthService.java
+++ src/main/java/com/zgamelogic/services/ModrinthService.java
@@ -1,10 +1,17 @@
 package com.zgamelogic.services;
 
+import com.zgamelogic.data.modrinth.ModrinthNotification;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.http.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
 
 @Service
 public class ModrinthService {
@@ -17,12 +24,24 @@ public class ModrinthService {
     }
 
     @PostConstruct
-    public void getNotifications(){
+    public List<ModrinthNotification> getNotifications(){
         String url = MODRINTH_API_URL + "/user/" + "zabory" + "/notifications";
         RestTemplate restTemplate = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.add("Authorization", token);
-        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, new HttpEntity<>(headers), String.class);
-        System.out.println(response.getBody());
+        ResponseEntity<ModrinthNotification[]> response = restTemplate.exchange(url, HttpMethod.GET, new HttpEntity<>(headers), ModrinthNotification[].class);
+        System.out.println(Arrays.toString(response.getBody()));
+        return List.of(response.getBody());
+    }
+
+    @Async
+    public void deleteNotifications(Collection<String> notificationIds){
+        String url = UriComponentsBuilder.fromUriString(MODRINTH_API_URL + "/notifications")
+            .queryParam("ids", String.join(",", notificationIds))
+            .toUriString();
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", token);
+        restTemplate.exchange(url, HttpMethod.DELETE, new HttpEntity<>(headers), ModrinthNotification[].class);
     }
 }
