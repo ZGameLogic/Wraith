a95b4dd23e5abd7cc1084db83fa4c65197325e2d
Ben Shabowski <benshabowski@gmail.com>
2025-09-11 09:29:45 -0500

    hot damn

---
+++ src/main/java/com/zgamelogic/services/AwsDebugService.java
@@ -0,0 +1,20 @@
+package com.zgamelogic.services;
+import jakarta.annotation.PostConstruct;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
+
+
+@Service
+@AllArgsConstructor
+public class AwsDebugService {
+    private final SecretsManagerClient  secretsManagerClient;
+
+    @PostConstruct
+    public void listAllSecrets() {
+        secretsManagerClient.listSecrets().secretList().forEach(s -> {
+            System.out.println(s.name());
+            System.out.println(secretsManagerClient.getSecretValue(r -> r.secretId(s.arn())).secretString());
+        });
+    }
+}
